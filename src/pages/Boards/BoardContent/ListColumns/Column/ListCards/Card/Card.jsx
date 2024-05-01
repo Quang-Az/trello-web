630a073e4ef07c5ae50a3764007f2b5524ca1215
QuangAnh109 <quangqb147@gmail.com>
2024-05-01 23:03:35 +0700

    trello board content p2

---
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -0,0 +1,58 @@
+import { Card as MuiCard } from '@mui/material'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import GroupIcon from '@mui/icons-material/Group'
+import CommentIcon from '@mui/icons-material/Comment'
+import AttachmentIcon from '@mui/icons-material/Attachment'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+
+function Card({ temporaryHideMedia }) {
+  if (temporaryHideMedia) {
+    return (
+      <MuiCard
+        sx={{
+          cursor: 'pointer',
+          boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
+          overflow: 'unset'
+        }}
+      >
+        <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>
+          <Typography>Card test </Typography>
+        </CardContent>
+      </MuiCard>
+    )
+  }
+  return (
+    <MuiCard
+      sx={{
+        cursor: 'pointer',
+        boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
+        overflow: 'unset'
+      }}
+    >
+      <CardMedia
+        sx={{ height: 140 }}
+        image="https://bienthuy.com/bienthuy-img/2020/04/pexels-photo-675920-scaled.jpeg"
+        title="green iguana"
+      />
+      <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>
+        <Typography>QuangAnhDev MERN Stack</Typography>
+      </CardContent>
+      <CardActions sx={{ p: '0 4px 8px 4px' }}>
+        <Button size="small" startIcon={<GroupIcon />}>
+          20
+        </Button>
+        <Button size="small" startIcon={<CommentIcon />}>
+          10
+        </Button>
+        <Button size="small" startIcon={<AttachmentIcon />}>
+          15
+        </Button>
+      </CardActions>
+    </MuiCard>
+  )
+}
+
+export default Card
